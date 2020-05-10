ed767c9d55ae0fa6e4866f9a7ca8cb6802fc05cd
Yassine ENNAJEM <yassineennajem68@gmail.com>
2020-05-10 02:45:04 +0000

    ngIf Directive & ngFor Directive

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -7,17 +7,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TestComponent implements OnInit {
 	// fileds
+	private ligne: number = 1;
 	private name: string = 'Yassine ENNAJEM';
   private imageUrl: string = 'assets/images/bookLogo.png';
-  
-  public getImageUrl() { return this.imageUrl }; // Method
-  get Name() { return this.name;} // Property
+  private bookList = [];
+  /*
+	private bookList: string[] = [
+		'Learn HTML5 & Css3',
+		'get Started With Angular 9',
+		'C" From Scratch',
+		'A key To A Future Java',
+		'Entity Framework Core',
+		'ASP.NET Core & Angular 9',
+		'best Practice From C#',
+		'Delegates C#'
+	];
+*/
+	public getImageUrl() {
+		return this.imageUrl;
+	} // Method
+	get Name() {
+		return this.name;
+	} // Property
+	get BookList() {
+		return this.bookList;
+	}
 
-  // Function 
-  public DisplayName(nameParameter:string) {
-    alert(`Hello ${nameParameter}`);
-  }
+	// Function
+	public DisplayName(nameParameter: string) {
+		alert(`Hello ${nameParameter}`);
+	}
 
+	// Function to Control Ligne in HTML
+	public PositionNumber() {
+		if (this.ligne % 2 === 0) {
+			this.ligne++;
+			return true;
+		} else {
+			this.ligne++;
+			return false;
+		}
+	}
 
 	constructor() {}
 
